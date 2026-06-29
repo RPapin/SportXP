fd57d565aa1ed4024c40e72a38aae5917ec2d224
RPapin <remipapin6@gmail.com>
2026-06-29 11:33:09 +0200

    Update environnement.prod to match the new domain

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://sport-xp-backend.vercel.app',
-  wsUrl: 'https://sport-xp-backend.vercel.app',
+  apiUrl: 'https://sport-xp.duckdns.org',
+  wsUrl: 'https://sport-xp.duckdns.org',
 };
